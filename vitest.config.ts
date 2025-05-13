20edcbac3cdc093cf32abd5c6fb630434ab3768d
Michael Verschoof <michael.verschoof@gmail.com>
2025-05-13 07:58:26 +0200

    Combined the configuration files

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,33 @@ export default mergeConfig(
     viteConfig,
     defineConfig({
         test: {
+            workspace: [
+                {
+                    extends: true,
+                    test: {
+                        name: 'unit',
+                        environment: 'happy-dom',
+                        include: ['./**/*.test.ts'],
+                        exclude: [...configDefaults.exclude, './**/*.browser.test.ts']
+                    }
+                },
+                {
+                    extends: true,
+
+                    test: {
+                        name: 'browser',
+                        include: ['./**/*.browser.test.ts'],
+                        browser: {
+                            provider: 'playwright',
+                            enabled: true,
+                            headless: true,
+                            screenshotFailures: false,
+                            instances: [{ browser: 'chromium' }]
+                        }
+                    }
+                }
+            ],
+
             environment: 'happy-dom',
             exclude: [...configDefaults.exclude, 'e2e/**'],
             root: fileURLToPath(new URL('./', import.meta.url)),
