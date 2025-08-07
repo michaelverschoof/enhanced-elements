da0fac4830278d34af51b37a5d6fb97367eb6d65
Michael Verschoof <michael.verschoof@gmail.com>
2025-08-07 10:30:14 +0200

    Filter out presets for filters and modifiers in numeric input as they apply to text

--- packages/inputs/src/components/numeric-input.vue
+++ packages/inputs/src/components/numeric-input.vue
@@ -1,24 +1,30 @@
 <template>
-    <textual-input ref="element" :filters="combinedFilters" :modifiers="modifiers" />
+    <textual-input ref="element" :filters="adjustedFilters" :modifiers="adjustedModifiers" />
 </template>
 
 <script setup lang="ts">
 import TextualInput from '@/components/textual-input.vue';
-import type { TransformableInputProps } from '@/components/types';
-import type { Filters } from '@/functions/model';
+import type { TransformableInputProps, ValidatableInputProps } from '@/components/types';
+import { filterPresets, Modifier, type Filter } from '@/functions/model';
 import { computed, InputHTMLAttributes, useTemplateRef } from 'vue';
 
 type Props = Omit</* @vue-ignore */ InputHTMLAttributes, 'type'> &
-    TransformableInputProps & { allowedCharacters?: string };
+    TransformableInputProps &
+    ValidatableInputProps & { allowedCharacters?: string };
 
 const { filters = [], modifiers = [], allowedCharacters = '' } = defineProps<Props>();
 
 const element = useTemplateRef<InstanceType<typeof TextualInput>>('element');
 
-const combinedFilters = computed<Filters[]>(() => [new RegExp(`[0-9${allowedCharacters}]`, 'g'), filters].flat());
+const adjustedFilters = computed<Filter[]>(() =>
+    [new RegExp(`[0-9${allowedCharacters}]`, 'g'), filterPresets<Filter>(filters)].flat()
+);
+
+const adjustedModifiers = computed<Modifier[]>(() => filterPresets<Modifier>(modifiers).flat());
 
 defineExpose({
     focus: () => element.value?.focus(),
-    blur: () => element.value?.blur()
+    blur: () => element.value?.blur(),
+    validate: () => element.value?.validate()
 });
 </script>
