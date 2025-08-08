ec0aac83138b099306ce9b0acbf3fba600b9d641
Michael Verschoof <michael.verschoof@gmail.com>
2025-08-08 09:05:46 +0200

    Initial working setup of tsdown

--- packages/inputs/src/types.ts
+++
@@ -1,7 +0,0 @@
-export type {
-    FocusableEmits,
-    TransformableInputProps,
-    ValidatableEmits,
-    ValidatableInputProps,
-    ValidationResult
-} from './components/types';
--- packages/inputs/vite.config.ts
+++ packages/inputs/vite.config.ts
@@ -10,22 +10,5 @@ export default defineConfig({
             '@test': fileURLToPath(new URL('./test', import.meta.url)),
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
-    },
-    build: {
-        target: 'esnext',
-        lib: {
-            entry: fileURLToPath(new URL('./index.ts', import.meta.url)),
-            name: '@enhanced-elements/inputs',
-            fileName: 'index'
-        },
-        rollupOptions: {
-            external: ['vue'],
-            output: {
-                exports: 'named',
-                globals: {
-                    vue: 'Vue'
-                }
-            }
-        }
     }
 });
