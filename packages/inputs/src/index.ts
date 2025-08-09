041defb9ce866f56c4a1c3db15cac82ce392b498
Michael Verschoof <michael.verschoof@gmail.com>
2025-08-09 18:45:12 +0200

    Updated index

--- packages/inputs/src/index.ts
+++ packages/inputs/src/index.ts
@@ -4,10 +4,17 @@ import { App } from 'vue';
  * Base components
  */
 import CheckableInput from './components/checkable-input.vue';
+import FileInput from './components/file-input.vue';
 import NumericInput from './components/numeric-input.vue';
+import PasswordInput from './components/password-input.vue';
 import RadioInput from './components/radio-input.vue';
 import TextualArea from './components/textual-area.vue';
 import TextualInput from './components/textual-input.vue';
+
+/**
+ * Component exports
+ */
+export { CheckableInput, FileInput, NumericInput, PasswordInput, RadioInput, TextualArea, TextualInput };
 
 export type {
     FocusableEmits,
@@ -18,17 +25,14 @@ export type {
 } from './components/types';
 
 /**
- * Component exports
- */
-export { CheckableInput, NumericInput, RadioInput, TextualArea, TextualInput };
-
-/**
  * Plugin
  */
 export default {
     install: (app: App) => {
         app.component('CheckableInput', CheckableInput);
+        app.component('FileInput', FileInput);
         app.component('NumericInput', NumericInput);
+        app.component('PasswordInput', PasswordInput);
         app.component('TextualInput', TextualInput);
         app.component('TextualArea', TextualArea);
         app.component('RadioInput', RadioInput);
